7a98c6020f90c3b8f148f66d3a73f85f4328bf11
Oleg Naumov <oleg.n@imobis.ru>
2022-05-20 23:47:57 +0300

    add recording indicator to statusbar, change logo

--- client/layouts/default.vue
+++ client/layouts/default.vue
@@ -3,7 +3,7 @@
     <b-navbar type="is-primary">
       <template #brand>
         <b-navbar-item tag="a" href="/">
-          <img src="/logo.png">
+          <img src="/brain_logo.webp" class="logo">
         </b-navbar-item>
       </template>
       <template #start>
@@ -28,11 +28,18 @@
     </b-navbar>
 
     <div class="container is-max-desktop">
-      <Nuxt />
+      <Nuxt/>
     </div>
 
     <div class="statusbar">
-      <span>{{ recordState }}</span>
+      <div>
+        <div v-if="recordState === 'ACTIVE'" class="ring-container mr-1">
+          <div class="ringring"></div>
+          <div class="circle"></div>
+        </div>
+        <b-icon v-else-if="recordState === 'PAUSED'" icon="pause" type="is-white" size="is-small"></b-icon>
+        <span>Запись {{ recordStateTexts }}</span>
+      </div>
     </div>
   </div>
 </template>
@@ -42,7 +49,8 @@ import { mapState } from 'vuex'
 
 export default {
   name: 'DefaultLayout',
-  data () {
+
+  data() {
     return {
       items: []
     }
@@ -51,7 +59,22 @@ export default {
   computed: {
     ...mapState('record', {
       recordState: state => state.recordState,
-    })
+    }),
+    ...mapState('device', {
+      startedDevices: state => state.startedDevices,
+    }),
+
+    recordStateTexts() {
+      switch (this.recordState) {
+        case 'INACTIVE':
+        case 'STOPPED':
+          return 'неактивна'
+        case 'ACTIVE':
+          return 'активна'
+        case 'PAUSED':
+          return 'приостановлена'
+      }
+    }
   }
 }
 </script>
@@ -65,6 +88,11 @@ export default {
   padding: 2.5rem 2rem;
 }
 
+.logo {
+  -webkit-filter: invert(100%);
+  filter: invert(100%);
+}
+
 .statusbar {
   position: fixed;
   bottom: 0;
@@ -73,5 +101,45 @@ export default {
   min-height: 30px;
   background-color: $primary;
   color: white;
+  display: flex;
+}
+
+.ring-container {
+  position: relative;
+  display: inline-block;
+}
+
+.circle {
+  width: 12px;
+  height: 12px;
+  background-color: $success;
+  border-radius: 50%;
+}
+
+.ringring {
+  border: 3px solid #62bd19;
+  -webkit-border-radius: 30px;
+  height: 22px;
+  width: 22px;
+  -webkit-animation: pulsate 1s ease-out;
+  -webkit-animation-iteration-count: infinite;
+  opacity: 0.0;
+  position: absolute;
+  top: -5px;
+  left: -5px;
+}
+
+@-webkit-keyframes pulsate {
+  0% {
+    -webkit-transform: scale(0.1, 0.1);
+    opacity: 0.0;
+  }
+  50% {
+    opacity: 1.0;
+  }
+  100% {
+    -webkit-transform: scale(1.2, 1.2);
+    opacity: 0.0;
+  }
 }
 </style>
